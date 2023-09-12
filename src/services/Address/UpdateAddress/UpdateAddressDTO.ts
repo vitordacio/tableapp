00000b2c824385db64ea0720498dc292e3948863
Vitor Dacio <vitordacio@hotmail.com>
2023-09-11 23:33:30 -0300

    install babel build plugin

--- src/services/Address/UpdateAddress/UpdateAddressDTO.ts
+++ src/services/Address/UpdateAddress/UpdateAddressDTO.ts
@@ -1,28 +1,10 @@
-// interface IAddress {
-//   zip: string;
-//   street: string;
-//   uf: string;
-//   city: string;
-//   district: string;
-//   number: string;
-// }
-
-interface IUpdateAddress {
-  name?: string;
-  // email?: string;
-  // phone?: string;
-  // address?: IAddress;
-  // document?: string;
-  // surname?: string;
-  // password?: string;
-  // permissions?: string[];
-  // is_locator?: boolean;
-}
-
 interface IUpdateAddressDTO {
-  fields: IUpdateAddress;
-  // AddressId: string;
-  requestAddress: ExpressUser;
+  zip?: string;
+  street?: string;
+  uf?: string;
+  city?: string;
+  district?: string;
+  number?: string;
 }
 
-export { IUpdateUserDTO, IUpdateUser };
+export { IUpdateAddressDTO };
